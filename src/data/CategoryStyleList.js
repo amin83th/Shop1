4d1c7171771f7e0f06779c65c01ed7c745220cfb
Porya Zarei <65762370+porya-zarei@users.noreply.github.com>
2021-10-03 18:03:40 +0330

    Update CategoryStyleList.js

--- src/data/CategoryStyleList.js
+++ src/data/CategoryStyleList.js
@@ -4,7 +4,20 @@ import {useState} from "react";
 function CategoryStyleList(props) {
   // const one = useRef();
 
-  const [before,setBefore] = useState("");
+  const [selectedMeetups,setSelectedMeetups] = useState([]); // const {selectedMeetups,setSelectedMeetups} = useContext(MainContext);
+  
+  const handleAddMeetup = (meetupId,checked)=>{
+    const isInSelecteds = selectedMeetups.includes(meetupId);
+    if(isInSelecteds && !checked){
+      let newSelectedMeetups = [...selectedMeetups];
+      const index = selectedMeetups.findIndex(m => m.Id === meetupId);
+      newSelectedMeetups.splice(index,1);
+      setSelectedMeetups(newSelectedMeetups);
+    }else if (!isInSelecteds && checked){
+      setSelectedMeetups(p => [...p,meetupId]);
+    }
+  }
+  
   function box(event){
     event.preventDefault();
     // const enteredOne = one.current.value;
@@ -19,7 +32,8 @@ function CategoryStyleList(props) {
       <form onSubmit={box}>
   ‍‍‍     {props.meetups.map(meetup => (
           <div key={meetup.Id}>
-            <button value={meetup.Title}>{meetup.Title}</button>
+            <label>{meetup.Title}</label>
+            <input type="checkbox" onChange={(e)=> handleAddMeetup(meetup.Id,e.target.checked)} />
           </div>
         ))}
       </form>
